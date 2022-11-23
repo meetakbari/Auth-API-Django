a9129993d4b025c5e009ea028ceb8372a6031a32
meetakbari <meetakbari.me@gmail.com>
2022-11-23 23:24:27 +0530

    user model created and migrated in core app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=50)
+
+    first_name = None
+    last_name = None
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [email, password]
